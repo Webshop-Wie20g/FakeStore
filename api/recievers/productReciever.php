e4d1d0702d5401469d3f5e37f60e0da14dc7780d
ShahinSayadi <Shahin_sayadi@hotmail.com>
2021-02-10 18:00:18 +0100

    Skapat filer för product sidan och gjort fetch samt enpoint

--- api/recievers/productReciever.php
+++ api/recievers/productReciever.php
@@ -1,1 +1,48 @@
-hello world
+<?php
+
+try{
+    if(isset($_SERVER["REQUEST_METHOD"])){
+
+        if($_SERVER["REQUEST_METHOD"] == "GET"){
+            
+            echo("hej hej");
+
+        }else{
+
+            throw new Exception("Wrong Method", 500);
+        }
+
+
+    }else{
+        
+        throw new Exception("No request method set", 500);
+    }
+
+}catch(Exception $error) {
+    echo json_encode(
+        array(
+            "Message" => $error -> getMessage(),
+            "Status" => $error -> getCode() 
+
+        )
+    );
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
