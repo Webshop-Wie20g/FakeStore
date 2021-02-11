8c7b2268d608610e3e4d70ec536e419f651187fe
ShahinSayadi <Shahin_sayadi@hotmail.com>
2021-02-11 15:23:25 +0100

    Skapa databas kopplingen och generella funktioner för querys

--- api/handlers/databaseHandler.php
+++ api/handlers/databaseHandler.php
@@ -1,1 +1,50 @@
-hello world
+<?php
+
+Class Database {
+
+    function __construct(){
+
+        $dns = "mysql:host=localhost;dbname=store";
+        $user = "root";
+        $pass = "root";
+
+        $this->db = new PDO($dns, $user, $pass);
+        $this->db-exec("set names utf8");
+    }
+
+    public $db;
+
+    private function prepareQuery($query){
+        return $this->db->prepare($query);
+        
+    }
+
+    public function runQuery($query, $entity) {
+        $preparedQuery = $this->prepareQuery($query);
+        $status = $preparedQuery->execute($entity);
+        return $status;
+    }
+
+    public function fetchQuery($query){
+        $preparedQuery = $this->prepareQuery($query);
+        $preparedQuery->execute();
+        return $preparedQuery->fetchAll(PDO::FETCH_OBJ);
+
+    }
+
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
--- product.js
+++ product.js
@@ -2,19 +2,30 @@ window.addEventListener("load", initsite)
 
 
 function initsite(){
-
-   
+    getAllProducts()
+    
 }
 
 async function getAllProducts(){
     const result = await makeRequest("./api/recievers/productReciever.php", "GET")
+    console.log(result)
+    
+   /*  if(result !=undefined){
+        result = JSON.parse(result)
 
+    }else{
+        
+        result = [] 
+        
+    } */
+    
+    /* console.log(result) */
 }
 
 async function makeRequest(url , method , body){
     try{
-        const respone = await fetch(url,{method , body})
-        return Response.json()
+        const response = await fetch(url,{method , body})
+        return response.json()
 
 
     }catch(err) {
