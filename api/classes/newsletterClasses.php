90860e44acd0c26dc4bae4b3b2d2713ea5eb3e88
Rikyan Barraza <victor.barrazamerino@medieinstitutet.se>
2021-03-30 07:24:23 +0200

    changes
    changes

---
+++ api/classes/newsletterClasses.php
@@ -0,0 +1,16 @@
+
+<?php
+
+class Newsletter {
+
+    function __construct() {
+        $this->id = intval($this->id);
+
+    }
+    public $id;
+    public $name;    
+    public $email;  
+    
+
+}
+?>
--- api/repositories/emailRepository.php
+++ api/repositories/emailRepository.php
@@ -3,6 +3,6 @@ require("./../handlers/databaseHandler.php");
 
 function addNewsletter($newsletter) {
      $db = new Database();
-     return $db->runQuery("INSERT INTO newsletter (Name, email) VALUES (:name, :email);", $newsletter);
+     return $db->runQuery("INSERT INTO newsletter (name, email) VALUES (:name, :email);", $newsletter);
 }
 ?>
